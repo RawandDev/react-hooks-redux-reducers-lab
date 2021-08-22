12347b2a7bb9452012ddb17ea5a95ca1b84eb319
RawandDev <rawand.200091384037@gmail.com>
2021-08-22 13:58:28 +0300

    done!

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,12 @@
 export function managePresents(state, action) {
   // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return {
+        ...state,
+        numberOfPresents: state.numberOfPresents + 1,
+      };
+    default:
+      return state;
+  }
 }
